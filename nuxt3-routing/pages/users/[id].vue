4e3a6b75a6cb9b445a11c8fce177ca8bc8a29541
yu00212 <yu2000212@icloud.com>
2023-06-27 22:41:19 +0900

    add：ルーティングについて

---
+++ nuxt3-routing/pages/users/[id].vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>users/{{ $route.params.id }}を表示中</h1>
+    </div>
+</template>
+<script setup>
+    // パラメータが整数かバリデーション
+    definePageMeta({
+        validate: async(route) => {
+            return /^\d+$/.test(route.params.id)
+        }
+    })
+</script>
