ad4bc9a0cc60841e5c999525edfafac30fc57041
yu00212 <yu2000212@icloud.com>
2023-06-27 22:15:52 +0900

    add：環境構築

---
+++ nuxt-first-exerci/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>Top Page</h1>
+        <hr>
+        <NuxtLink to="/mypage">My Page</NuxtLink>
+    </div>
+</template>
---
+++ nuxt-first-exerci/pages/mypage.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>My Page</h1>
+        <hr>
+        <NuxtLink to="/">Top Page</NuxtLink>
+    </div>
+</template>
