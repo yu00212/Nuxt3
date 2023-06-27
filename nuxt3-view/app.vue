d491edb2ae16a7f6a041603506bf5eb101c4d1d8
yu00212 <yu2000212@icloud.com>
2023-06-27 23:11:10 +0900

    add：ビューについて

---
+++ nuxt3-view/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+<script setup>
+useHead(
+  {
+    title: "Nuxt 3 Store",
+    link: [
+      {rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap"}
+    ]
+  }
+)
+</script>
---
+++ nuxt3-view/error.vue
@@ -0,0 +1,10 @@
+<template>
+    <div v-if="error.statusCode === 404">
+        <h1>Error!</h1>
+        <p>{{ error.message }}</p>
+    </div>
+</template>
+<script setup>
+const error = useError()
+console.log(error)
+</script>
