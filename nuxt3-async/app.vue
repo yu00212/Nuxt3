1dbecbbf9ce34fa77736e1c23c0c2e67c1585f2b
yu00212 <yu2000212@icloud.com>
2023-06-27 23:45:24 +0900

    add:非同期データ通信について

---
+++ nuxt3-async/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.id }}, {{ user.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script setup>
+  const { data: users, error } = await useFetch('https://jsonplaceholicode.com/users')
+  // console.log(error.value)
+  if(error.value) {
+    throw createError({ statusCode: 404, statusMessage: 'Page not found!!!'})
+  }
+</script>
